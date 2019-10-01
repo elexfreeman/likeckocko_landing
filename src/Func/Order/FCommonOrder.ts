bab73362d334cdc4604c12b4823d1bec813b426c
elexfreeman <elextraza@gmail.com>
2019-10-01 21:05:52 +0400

    calc summ

--- src/Func/Order/FCommonOrder.ts
+++ src/Func/Order/FCommonOrder.ts
@@ -3,6 +3,13 @@ import * as SFunc from "../Sys/SFunc";
 import * as TFOrder from "./TOrder";
 
 export const fCalcOrderTotalSumm: TFOrder.TCalcOrderTotalSumm =
-(aProducts: TFOrder.OrderProductI[]) =>
-    aProducts.map(product => product.count * product.price)
+(aProducts: TFOrder.OrderProductI[]) => {
+    if(aProducts.length> 0) {
+        return aProducts.map(product => product.count * product.price)
         .reduce(SFunc.fSum); // массив цены за товар
+    } else {
+        return 0;
+    }
+    
+}
+    
