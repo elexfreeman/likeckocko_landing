a43691f7c6d331b25cdbc1c0b414380484e3faf4
elexfreeman <elextraza@gmail.com>
2020-04-12 14:22:12 +0300

    СТр товара

--- src/Pages/CartP.ts
+++ src/Pages/CartP.ts
@@ -1,7 +1,7 @@
 import { MainRequest } from "../Func/Sys/MainRequest";
 import * as express from 'express';
 import { faSendRouter } from "../Func/Sys/FResponse";
-import { IndexR as R} from "./Router"
+import { CartR as R} from "./Router"
 
 const router = express.Router();
 
