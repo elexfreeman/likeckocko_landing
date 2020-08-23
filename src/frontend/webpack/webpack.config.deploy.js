397c0565b83b3ce19c3ce59e4a17834440c0c790
alex <AAnPolozov@sberbank.ru>
2020-08-23 13:30:07 +0300

    trufels

--- src/frontend/webpack/webpack.config.deploy.js
+++ src/frontend/webpack/webpack.config.deploy.js
@@ -48,7 +48,7 @@ module.exports = {
         },
         {
             test: /\.scss$/,
-            use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            use: ['style-loader', 'css-loader', 'sass-loader']
 
         },
         {
